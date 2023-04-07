9f8cc344ddeeeb574eee41e579ce01e8deb5a286
luisvzq <122988196+luisvzq@users.noreply.github.com>
2023-04-07 18:11:08 +0200

    Dia 1

---
+++ documentacion1/ejemplo1.js
@@ -0,0 +1,21 @@
+
+// let variables que van a cambiar 
+// const variables constantes que nunca cambian
+// string texto
+let miVariable = "Hola" 
+// int numero
+let miVariable2 = 6
+miVariable2.toString() // convertir numero a text
+let miVariable5 = 12
+
+// boolean
+let miVariable3 = false || true
+miVariable = "15"
+parseInt(miVariable, 10) // convertir texto a numero 
+
+function suma (numero1, numero2){
+    const resultado = numero1 + numero2
+    return resultado
+}
+
+console.log(suma(14, 15))
